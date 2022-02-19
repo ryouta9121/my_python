e31f90dd09b455a61e0a05aacf74b1ca8b5dd2c9
ryouta9121 <nyagoro33@gmail.com>
2022-02-19 23:05:36 +0900

    変数

--- Good.py
+++ Good.py
@@ -1,2 +1,19 @@
 print("Hello World!!")
 
+a = 120 
+print (type(a))
+
+b = 4.5 
+print (type(b))
+
+complex = 3j
+print(type(complex))
+
+name = "吉川"
+print(type(name))
+
+flg = True
+print(type(flg))
+
+c = [10, 20]
+print(type(c))
