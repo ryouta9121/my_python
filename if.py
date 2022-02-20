234b87577c48660ad71614c3553221f5c0e03a4a
ryouta9121 <nyagoro33@gmail.com>
2022-02-20 12:12:32 +0900

    if

---
+++ if.py
@@ -0,0 +1,15 @@
+# money = 1000
+
+# if money >= 2000:
+#     print("映画館に行きます")
+# else:
+#     print("映画館には行きません")
+
+money = 3000
+
+if money >= 8000:
+    print("ディズニーランドに行く")
+elif money >= 2000:
+    print("映画館に行きます")
+else:
+    print("どこにも行きません")
