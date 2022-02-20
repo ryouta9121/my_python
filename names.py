214f8b82aac9c87336c55a2bdf628a67a358df45
ryouta9121 <nyagoro33@gmail.com>
2022-02-20 11:56:37 +0900

    dictionary&tuple

--- names.py
+++ names.py
@@ -6,6 +6,8 @@
 # print (names[0])
 # print (names[1])
 
+
+
 scores = [50,80,60]
 
 
@@ -15,8 +17,40 @@ scores = [50,80,60]
 # print (scores[:-1])
 
 scores.append(70)
-scores.append(90)
+# # scores.append(90)
+# # print (scores)
+
+scores.pop(0)
 print (scores)
+
+# del scores[1]
+# print (scores)
+
+# del scores[0]
+# print (scores)
+
+scores = {'国語':50,'数学':80,'英語':60}
+
+print (scores['国語'])
+
+scores['理科'] = 70
+
+print (scores['理科'])
+print (scores)
+
+scores['社会'] = 100
+
+scores = (10,30,40)
+
+
+
+
+
+
+
+
+
+
 
 
 
