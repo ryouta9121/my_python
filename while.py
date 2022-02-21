568be90f195a0e3e9c050b1261d1e69afe5e6ab7
ryouta9121 <nyagoro33@gmail.com>
2022-02-21 18:57:30 +0900

    while break & contiune

--- while.py
+++ while.py
@@ -1,6 +1,22 @@
-time = 60
-while time>0:
-    print("残り",time,"秒")
-    time -= 1
+# time = 60
+# while time>0:
+#     print("残り",time,"秒")
+#     time -= 1
 
-print("試合終了")
+# print("試合終了")
+
+i = 0
+
+# while True:
+#     i += 1
+#     if i == 50:
+#         break
+#     print(i)
+
+while True:
+    i += 1
+    if i == 50:
+        continue
+    elif i>100:
+        break
+    print(i)
