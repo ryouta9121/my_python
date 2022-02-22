7247e0053f437b27dda29b1669dbff54ea5228ea
ryouta9121 <nyagoro33@gmail.com>
2022-02-22 23:50:55 +0900

    function return

---
+++ function.py
@@ -0,0 +1,17 @@
+# def say_hello():
+#     print("みなさん、こんにちわ。")
+ 
+# say_hello()
+
+
+# def say_hello2(name):
+#     print("{}さん、こんにちわ。".format(name))
+
+# say_hello2("今西")
+
+
+def calc_square(side):
+   return side*side
+
+calc_square(10)
+
